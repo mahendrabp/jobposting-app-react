5501234ee408a2337bfb07c48c6723635ffb9808
mahendrabp <chipxitro@gmail.com>
2019-12-09 11:47:27 +0700

    Edit: test non active logger redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import promiseMiddleware from 'redux-promise-middleware';
 
 import rootReducer from './reducer';
 
-const logger = createLogger();
+// const logger = createLogger();
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, promiseMiddleware)
+  applyMiddleware(
+    //logger,
+    promiseMiddleware
+  )
 );
 
 export default store;
